b58ab1dc5418aa0b6eab368facdc05500750eac2
Ashaduzzaman Faruque <ashaduzzaman5698@gmail.com>
2025-08-28 18:56:33 +0600

    divided into components

---
+++ Tutorial-12/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+  <header class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+    <div class="container">
+      <a class="navbar-brand fw-bold" href="#">
+        <i class="bi bi-check2-square text-primary me-1"></i> Todo App
+      </a>
+
+      <button class="btn btn-primary rounded-circle ms-auto" data-bs-toggle="modal"
+              data-bs-target="#addTodoModal">
+        +
+      </button>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
--- Tutorial-12/src/components/Todo.vue
+++ Tutorial-12/src/components/Todo.vue
@@ -1,8 +1,14 @@
 <script>
-
+import Header from './Header.vue'
+import TaskModal from './TaskModal.vue'
+import ShowTask from './ShowTask.vue'
 
 export default {
-  props:{
+  components:{
+    Header,
+    TaskModal,
+    ShowTask
+  },props:{
     todoList:Object
   },data(){
     return{
@@ -31,54 +37,16 @@ export default {
 
 <template>
   <div class="container">
-    <header class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-      <div class="container">
-        <a class="navbar-brand fw-bold" href="#">
-          <i class="bi bi-check2-square text-primary me-1"></i> Todo App
-        </a>
-
-        <button class="btn btn-primary rounded-circle ms-auto" data-bs-toggle="modal"
-                data-bs-target="#addTodoModal">
-          +
-        </button>
-      </div>
-    </header>
-
-    <div class="modal fade" id="addTodoModal" tabindex="-1" aria-labelledby="addTodoLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content rounded-4 shadow">
-          <div class="modal-header">
-            <h5 class="modal-title" id="addTodoLabel">Add New Todo</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div class="row m-2">
-              <input type="text" class="form-control" v-model="title" placeholder="Enter your task title">
-            </div>
-            <div class="row m-2">
-              <textarea class="form-control" v-model="description" placeholder="Enter your task..." cols="6" rows="6"></textarea>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-primary" @click="addTask">Add</button>
-          </div>
-        </div>
-      </div>
+   <Header/>
+  <TaskModal @addTask="addTask"/>
+    <div class="row">
+      <ShowTask
+          v-for="item in tasks"
+          :key="item.id"
+          :item="item"
+          @removeTask="removeTask(item.id)"
+      />
     </div>
-
-   <div class="row">
-     <div class="mt-4 col-md-4" v-for="item in tasks">
-       <div class="card text-white" style="width: 18rem; background: darkcyan;">
-         <div class="card-body">
-           <h5 class="card-title text-black"><b>{{ item.title }}</b></h5>
-           <p class="card-text">{{ item.description }}</p>
-           <button type="button" @click="removeTask(item.id)" class="btn btn-danger">Remove</button>
-         </div>
-       </div>
-     </div>
-   </div>
-
   </div>
 </template>
 
