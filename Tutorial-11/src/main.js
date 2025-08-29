ab3c6a620576468241ba092c9ee52a7bb8267b60
Ashaduzzaman Faruque <ashaduzzaman5698@gmail.com>
2025-08-29 16:42:07 +0600

    Optional API

--- Tutorial-11/src/main.js
+++
@@ -1,8 +0,0 @@
-
-import { createApp } from 'vue'
-import App from "@/App.vue";
-import Counter from "@/components/Counter.vue";
-
-const app = createApp(App);
-app.component('Counter', Counter)
-app.mount('#app')
--- Tutorial-15/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './app.vue'
-import router from './route.js'
-
-createApp(App)
-    .use(router)
-    .mount('#app')
