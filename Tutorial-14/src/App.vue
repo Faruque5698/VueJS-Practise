4960ff7f94a3eb25adcb2c5bb3235526e461e1c3
Ashaduzzaman Faruque <ashaduzzaman5698@gmail.com>
2025-08-29 14:14:25 +0600

    V-model with components

---
+++ Tutorial-14/src/App.vue
@@ -0,0 +1,50 @@
+<script>
+import InputField from "@/components/InputField.vue";
+import InputFieldV2 from "@/components/InputFieldV2.vue";
+import InputFieldV3 from "@/components/InputFieldV3.vue";
+
+export default {
+  components: {
+    InputField,
+    InputFieldV2,
+    InputFieldV3
+  },
+  data() {
+    return {
+      msg: '',
+      msgV2: '',
+      name:'',
+      email:'',
+      password:'',
+      textMessage:''
+
+    }
+  },
+  updated() {
+    this.textMessage = 'My name is ' + this.name + ', email is ' + this.email + ' and password is ' + this.password;
+  }
+};
+</script>
+
+<template>
+<!--  {{ msg }}-->
+<!--  <InputField-->
+<!--      label-value="Email"-->
+<!--      :input-value="msg"-->
+<!--      @input-i="(value) => msg = value"-->
+<!--  />-->
+<!--  <br> <br>-->
+
+<!--  {{ msgV2 }}-->
+
+<!--  <InputFieldV2 label-value="Password" v-model="msgV2"  />-->
+
+  <InputFieldV3 v-model:nameVal="name" v-model:emailVal="email" v-model:passwordVal="password" />
+  <br><br>
+  {{ textMessage }}
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Tutorial-14/src/components/InputFieldV2.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props:{
+    labelValue:String,
+    modelValue:String,
+  },
+  emits:["update:modelValue"],
+}
+</script>
+
+<template>
+  <label>{{ labelValue }}</label>
+  <input type="text" :value="modelValue" @input="$emit('update:modelValue' , $event.target.value)" />
+</template>
+
+<style scoped>
+
+</style>
